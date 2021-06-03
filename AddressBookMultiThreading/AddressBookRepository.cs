a3a3ce25d3544142b47f5cf947c2372db5c73858
shivammathur4 <shivamm.4aug@gmail.com>
2021-06-03 10:30:19 +0530

    Added method to retrieve Data

---
+++ AddressBookMultiThreading/AddressBookRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMultiThreading
+{
+    public class AddressBookRepository
+    {
+        public static SqlConnection connection { get; set; }
+        
+        public void RetrieveAllContactDetails()
+        {
+            DBConnection dbc = new DBConnection();
+            connection = dbc.GetConnection();
+            AddressBookModel model = new AddressBookModel();
+            try
+            {
+                using (connection)
+                {
+                    string query = @"select * from dbo.Address_Book";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.FirstName = reader.GetString(0);
+                            model.LastName = reader.GetString(1);
+                            model.Address = reader.GetString(2);
+                            model.City = reader.GetString(3);
+                            model.State = reader.GetString(4);
+                            model.Zip = reader.GetInt32(5);
+                            model.PhoneNumber = reader.GetInt64(6);
+                            model.EmailId = reader.GetString(7);
+                            model.AddressBookType = reader.GetString(8);
+                            model.AddressBookName = reader.GetString(9);
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}", model.FirstName, model.LastName,
+                                model.Address, model.City, model.State, model.Zip, model.PhoneNumber, model.EmailId, model.AddressBookType, model.AddressBookName);
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ AddressBookMultiThreading/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookMultiThreading
+{
+    public class DBConnection
+    {
+        public SqlConnection GetConnection()
+        {
+            string connectiongString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectiongString);
+            return connection;
+        }
+    }
+}
